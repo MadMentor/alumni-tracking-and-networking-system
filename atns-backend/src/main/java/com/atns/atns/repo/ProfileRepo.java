ce38dd437da10245b31101725912c2347cd21e9e
Faithful Crow 778 <sumitsainju@gmail.com>
2025-07-20 21:45:36 +0545

    Added a new query: findBySkill

--- atns-backend/src/main/java/com/atns/atns/repo/ProfileRepo.java
+++ atns-backend/src/main/java/com/atns/atns/repo/ProfileRepo.java
@@ -3,11 +3,17 @@ package com.atns.atns.repo;
 import com.atns.atns.entity.Profile;
 import com.atns.atns.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 @Repository
 public interface ProfileRepo extends JpaRepository<Profile, Integer> {
     Optional<Profile> findByUser(User user);
+
+    @Query("SELECT p from Profile p JOIN p.skills s WHERE LOWER(s.name) LIKE LOWER(CONCAT('%', :skill, '%'))")
+    Page<Profile> findBySkill(@Param("skill") String skill, Pageable pageable);
 }
