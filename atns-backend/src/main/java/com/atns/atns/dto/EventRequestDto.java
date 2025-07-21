c33d235f737f738b11b70982f0c678edd0103f0d
Faithful Crow 778 <sumitsainju@gmail.com>
2025-07-21 22:13:50 +0545

    Created EventRequestDto

---
+++ atns-backend/src/main/java/com/atns/atns/dto/EventRequestDto.java
@@ -0,0 +1,34 @@
+package com.atns.atns.dto;
+
+import com.atns.atns.entity.EventLocation;
+import com.atns.atns.entity.Profile;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventRequestDto {
+    @NotBlank(message = "Event title is required")
+    private String eventName;
+
+    private String eventDescription;
+
+    private EventLocationDto eventLocation;
+    @FutureOrPresent(message = "Start time must be in future")
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private String category;
+
+    @NotBlank(message = "Organization Profile Id is mandatory")
+    private Integer organizerProfileId;
+}
