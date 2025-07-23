a37270f0f44a8efc8bdb3078bb5518d983b69092
Faithful Crow 778 <sumitsainju@gmail.com>
2025-07-23 22:20:26 +0545

    Temporarily commented it to remove some exception

--- atns-backend/src/main/java/com/atns/atns/config/ProfileDataLoader.java
+++ atns-backend/src/main/java/com/atns/atns/config/ProfileDataLoader.java
@@ -1,42 +1,45 @@
-package com.atns.atns.config;
-
-import com.atns.atns.entity.Profile;
-import com.atns.atns.entity.User;
-import com.atns.atns.repo.ProfileRepo;
-import com.atns.atns.repo.UserRepo;
-import jakarta.transaction.Transactional;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDate;
-import java.util.Optional;
-
-@Configuration
-public class ProfileDataLoader {
-
-    @Bean
-    @Transactional
-    public CommandLineRunner loadProfiles(UserRepo userRepo, ProfileRepo profileRepo) {
-        return args -> {
-            Optional<User> optionalUser = Optional.ofNullable(userRepo.findByEmail("sumitsainju@gmail.com"));
-
-            if (optionalUser.isPresent() && profileRepo.count() == 0) {
-                User user = optionalUser.get();
-
-                Profile profile = Profile.builder()
-                        .firstName("Sumit")
-                        .lastName("Sainju")
-                        .bio("Java Developer")
-                        .phoneNumber("982358695")
-                        .address("Jadibuti")
-                        .dateOfBirth(LocalDate.of(2001, 12, 13))
-                        .user(user)
-                        .build();
-
-                profileRepo.save(profile);
-                System.out.println("Dummy profile inserted for user: " + user.getUsername());
-            }
-        };
-    }
-}
+//package com.atns.atns.config;
+//
+//import com.atns.atns.entity.Profile;
+//import com.atns.atns.entity.User;
+//import com.atns.atns.repo.ProfileRepo;
+//import com.atns.atns.repo.UserRepo;
+//import jakarta.transaction.Transactional;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.util.Optional;
+//
+//@Configuration
+//public class ProfileDataLoader {
+//
+//    @Bean
+//    @Transactional
+//    public CommandLineRunner loadProfiles(UserRepo userRepo, ProfileRepo profileRepo) {
+//        return args -> {
+//            Optional<User> optionalUser = Optional.ofNullable(userRepo.findByEmail("sumitsainju@gmail.com"));
+//
+//            if (optionalUser.isPresent() && profileRepo.count() == 0) {
+//                User user = optionalUser.get();
+//
+//                Profile profile = Profile.builder()
+//                        .firstName("Sumit")
+//                        .lastName("Sainju")
+//                        .bio("Java Developer")
+//                        .phoneNumber("9823586959")
+//                        .address("Jadibuti")
+//                        .dateOfBirth(LocalDate.of(2001, 12, 13))
+//                        .createdAt(LocalDateTime.now())
+//                        .profileImageUrl("\"C:\\Users\\Dell\\Downloads\\1320552.jpeg\"")
+//                        .user(user)
+//                        .build();
+//
+//                profileRepo.save(profile);
+//                System.out.println("Dummy profile inserted for user: " + user.getUsername());
+//            }
+//        };
+//    }
+//}
