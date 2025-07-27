1a03b1c12c7236dcf6e9a6fa822a115d7a72a1e1
Faithful Crow 778 <sumitsainju@gmail.com>
2025-07-27 12:03:48 +0545

    feat(auditing): add @AuditLog annotation for audit logging

---
+++ atns-backend/src/main/java/com/atns/atns/annotation/AuditLog.java
@@ -0,0 +1,13 @@
+package com.atns.atns.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuditLog {
+    String action() default "";
+    String entityType() default "";
+}
