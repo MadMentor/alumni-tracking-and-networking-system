85f3535d700312804244e753e91e608fdcc2f98a
Faithful Crow 778 <sumitsainju@gmail.com>
2025-07-29 22:41:19 +0545

    chore(eventService): remove unused import

--- atns-backend/src/main/java/com/atns/atns/service/EventService.java
+++ atns-backend/src/main/java/com/atns/atns/service/EventService.java
@@ -7,8 +7,6 @@ import com.atns.atns.entity.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface EventService {
     // Create Operations
     Event createEvent(EventRequestDto eventRequestDto, Integer organizerProfileId);
