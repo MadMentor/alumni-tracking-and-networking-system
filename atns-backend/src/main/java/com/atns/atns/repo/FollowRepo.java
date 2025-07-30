66daafea4423f6ebfcc3802ef0f860cdbc3baf6f
Faithful Crow 778 <sumitsainju@gmail.com>
2025-07-31 00:04:46 +0545

    feat(followRepo): add query to find followed profile

--- atns-backend/src/main/java/com/atns/atns/repo/FollowRepo.java
+++ atns-backend/src/main/java/com/atns/atns/repo/FollowRepo.java
@@ -14,9 +14,12 @@ import org.springframework.stereotype.Repository;
 public interface FollowRepo extends JpaRepository<Follow, Integer> {
     boolean existsByFollowerAndFollowed(Profile follower, Profile followed);
     void deleteByFollowerAndFollowed(Profile follower, Profile followed);
-    long countByFollowed(Profile profile);
-    long countByFollower(Profile profile);
+    int countByFollowed(Profile profile);
+    int countByFollower(Profile profile);
 
     @Query("SELECT f.follower FROM Follow f WHERE f.followed = :profile")
     Page<Profile> findByFollowed(@Param("profile") Profile profile, Pageable pageable);
+
+    @Query("SELECT f.follwed FROM Follow f WHERE f.follower =: profile")
+    Page<Profile> findByFollower(@Param("profile") Profile profile, Pageable pageable);
 }
