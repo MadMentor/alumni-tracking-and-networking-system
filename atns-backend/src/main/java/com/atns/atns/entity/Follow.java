964e623b98b4029c4dcfa392587ddd3b815f1a6b
Faithful Crow 778 <sumitsainju@gmail.com>
2025-07-30 23:36:41 +0545

    feat(follow): add Follow entity to manage follower-following relationships

---
+++ atns-backend/src/main/java/com/atns/atns/entity/Follow.java
@@ -0,0 +1,33 @@
+package com.atns.atns.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "follows")
+public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "follower_od")
+    private Profile follower;
+
+    @ManyToOne
+    @JoinColumn(name = "followed_id")
+    private Profile followed;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
