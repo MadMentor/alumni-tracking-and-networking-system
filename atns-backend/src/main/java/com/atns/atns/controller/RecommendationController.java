9b090b886cf462d1c3b274cc530d5e519fa62c3c
Faithful Crow 778 <sumitsainju@gmail.com>
2025-09-02 21:21:02 +0545

    feat(recommendation): add RecommendationController

---
+++ atns-backend/src/main/java/com/atns/atns/controller/RecommendationController.java
@@ -0,0 +1,42 @@
+package com.atns.atns.controller;
+
+import com.atns.atns.recommendation.RecommendationService;
+import com.atns.atns.recommendation.dto.RecommendedEventDto;
+import com.atns.atns.recommendation.dto.RecommendedUserDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/recommendations")
+@RequiredArgsConstructor
+public class RecommendationController {
+
+    private final RecommendationService recommendationService;
+
+    @GetMapping("/events/{profileId}")
+    public ResponseEntity<List<RecommendedEventDto>> getRecommendedEvents(@PathVariable Integer profileId,
+                                                                          @RequestParam(defaultValue = "10") int limit) {
+        log.info("Fetching recommended events for profile id {}, limit={}", profileId, limit);
+        List<RecommendedEventDto> recommendations = recommendationService.recommendEvents(profileId, limit);
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.noStore()) // optional, no caching
+                .body(recommendations);
+    }
+
+    @GetMapping("/users/{profileId}")
+    public ResponseEntity<List<RecommendedUserDto>> getRecommendedUsers(
+            @PathVariable Integer profileId,
+            @RequestParam(defaultValue = "10") int limit) {
+        log.info("Fetching recommended users for profileId={}, limit={}", profileId, limit);
+        List<RecommendedUserDto> recommendations = recommendationService.recommendUsers(profileId, limit);
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.noStore())
+                .body(recommendations);
+    }
+}
